1a908a65675f5f060ca1cdc90920ea88967e0248
Kennik89 <kand89@gmail.com>
2017-11-13 15:25:08 +0100

    Test file added

--- src/MicroC_language/MicroC.java
+++ src/MicroC_language/MicroC.java
@@ -11,16 +11,17 @@ import MicroC_language.parsing.*;
 public class MicroC {
 
 	public static void main(String args[]) throws Exception {
-		if (args.length == 0) {
-			System.out.println("Error: No program specified.");
-			return;
-		}
-        MicroCLexer lex = new MicroCLexer(new ANTLRFileStream(args[0]));
+//		if (args.length == 0) {
+//			System.out.println("Error: No program specified.");
+//			return;
+//		}
+        // MicroCLexer lex = new MicroCLexer(new ANTLRFileStream(args[0]));
+        MicroCLexer lex = new MicroCLexer(new ANTLRFileStream("test/test1.microC"));
+        System.out.println("Read file successful");
         CommonTokenStream tokens = new CommonTokenStream(lex);
         MicroCParser parser = new MicroCParser(tokens);
 		parser.program(); //This command parses the program.
-		
-		// Test
+		System.out.println("Program terminated");
        
 	}
 }
